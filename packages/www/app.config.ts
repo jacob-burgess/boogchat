21f3ede5f6ab92ac49abf74af4418bf2d73038f1
Jacob Burgess <jacobburgess11@gmail.com>
2025-02-01 12:14:00 -0800

    app config

--- packages/www/app.config.ts
+++ packages/www/app.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    preset: "aws-lambda",
+    awsLambda: {
+      streaming: true,
+    },
+  },
   vite: {
     plugins: [
       tsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
+      tailwindcss(),
     ],
   },
 });
