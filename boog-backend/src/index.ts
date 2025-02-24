048bb4fa44b38a2d0b21ae59853d6549d7eb7121
Jacob Burgess <jacobburgess11@gmail.com>
2025-02-24 12:53:58 -0800

    websocket connection

--- boog-backend/src/index.ts
+++ boog-backend/src/index.ts
@@ -1,21 +1,31 @@
 import { DurableObject } from 'cloudflare:workers';
 
 export class BoogChat extends DurableObject<Env> {
+	private sessions: Map<WebSocket, any>;
+
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
+
+		this.sessions = new Map();
+		this.ctx.getWebSockets().forEach((ws) => {
+			this.sessions.set(ws, { ...ws.deserializeAttachment() });
+		});
 	}
 
-	async sayHello(name: string): Promise<string> {
-		return `Hello, ${name}!`;
+	async fetch(_request: Request): Promise<Response> {
+		const pair = new WebSocketPair();
+		this.ctx.acceptWebSocket(pair[1]);
+		this.sessions.set(pair[1], {});
+		return new Response(null, { status: 101, webSocket: pair[0] });
 	}
 }
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request, env, _ctx): Promise<Response> {
+		// match the pathname to the appropriate durable object
 		let id: DurableObjectId = env.BOOG_CHAT.idFromName(new URL(request.url).pathname);
-		let stub = env.BOOG_CHAT.get(id);
-		let greeting = await stub.sayHello('world');
 
-		return new Response(greeting);
+		const boogchat = env.BOOG_CHAT.get(id);
+		return boogchat.fetch(request);
 	},
 } satisfies ExportedHandler<Env>;
